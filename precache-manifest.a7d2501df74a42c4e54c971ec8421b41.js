633ed9c3c5b9ff78665314a18659ae456f100823
Kushant Garg <71939372+kushantgarg@users.noreply.github.com>
2022-11-02 21:44:00 +0530

    Updates

---
+++ precache-manifest.a7d2501df74a42c4e54c971ec8421b41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba5ca9b415857889e6ba",
+    "url": "/my-app/static/css/main.1c978fc3.chunk.css"
+  },
+  {
+    "revision": "ba5ca9b415857889e6ba",
+    "url": "/my-app/static/js/main.ba5ca9b4.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/my-app/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "31d77044af729c125605",
+    "url": "/my-app/static/js/2.31d77044.chunk.js"
+  },
+  {
+    "revision": "99b7f12dffb4a4f3de0c",
+    "url": "/my-app/static/js/runtime~main.99b7f12d.js"
+  },
+  {
+    "revision": "f6d205e6d2893dec0d7c6c0c61db6ea8",
+    "url": "/my-app/index.html"
+  }
+];
